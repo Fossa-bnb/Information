3b6fd7796b2ab63d5edd77f0859119630a8fbc66
EvaFro <frolli.erin@gmail.com>
2018-03-05 11:00:07 -0800

    Added hostInfo & neighborhood comps

--- client/app/IndexPart2.jsx
+++ client/app/IndexPart2.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import RenderDOM from 'react-dom';
+import HostInfo from './components/HostInfo';
+import Neighborhood from './components/Neighborhood';
+
 
 const App = () => (
   <div>
     <h1>Further Info</h1>
-   
+    <HostInfo />
+    <Neighborhood />
   </div>
 );
 
