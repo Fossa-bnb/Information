65fbb02090d97716c14aa18bccc8919e2f4587b9
EvaFro <frolli.erin@gmail.com>
2018-03-05 10:52:09 -0800

    Imported room comps

--- client/app/Index.jsx
+++ client/app/Index.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import RenderDOM from 'react-dom';
+import Amenites from './components/Amenities';
+import CancelPolicy from './components/CancelPolicy';
+import HostImage from './components/HostImage';
+import HouseRules from './components/HouseRules';
+import RoomDescription from './components/RoomDescription';
+import RoomHeader from './components/RoomHeader';
+import SleepingArrangments from './components/SleepingArrangments';
 
 const App = () => (
-  <p>This Rendered</p>
+  <div>
+    <h1>My Room</h1>
+    <RoomHeader />
+    <HostImage />
+    <RoomDescription />
+    <Amenites />
+    <SleepingArrangments />
+    <HouseRules />
+    <CancelPolicy />
+
+
+  </div>
 );
 
 RenderDOM.render(<App />, document.getElementById('app'));
