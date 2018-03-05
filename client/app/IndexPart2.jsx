24195d28e7934c17247ceb95fe8d1c04cdaf81e7
EvaFro <frolli.erin@gmail.com>
2018-03-05 10:56:46 -0800

    Created 2nd index 2 house info comp after reviews

---
+++ client/app/IndexPart2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RenderDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Further Info</h1>
+   
+  </div>
+);
+
+RenderDOM.render(<App />, document.getElementById('app2'));
