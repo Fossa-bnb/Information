3cb27eec770904cbf36ca92adcaf88b461f26885
EvaFro <frolli.erin@gmail.com>
2018-03-05 16:08:57 -0800

    Fix test description

--- src/neighborhood.test.jsx
+++ src/neighborhood.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Neighborhood from '../client/components/neighborhood';
 
 describe('<Neighborhood /> Component', () => {
-  it('should render a title', () => {
+  it('should render a div', () => {
     const wrapper = shallow(<Neighborhood />);
     expect(wrapper.is('div')).toEqual(true);
   });
