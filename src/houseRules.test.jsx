b99c1289d4e8e8287c4f05574fe01a328baa6abf
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:01:52 -0800

    add houseRules paragraph test

--- src/houseRules.test.jsx
+++ src/houseRules.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import HouseRules from '../client/components/houseRules';
+import HouseRules from '../client/app/components/HouseRules';
 
 describe('<HouseRules /> Component', () => {
   it('should render a div', () => {
-    const wrapper = shallow(<HouseRules />);
+    const wrapper = shallow(<HouseRules room="" />);
     expect(wrapper.is('div')).toEqual(true);
+  });
+  it('renders the house rules paragraph given', () => {
+    const room = { houseRules: 'House rules are...' };
+    const wrapper = shallow(<HouseRules room={room} />);
+    // need to figure out how to test this componit
+    expect(wrapper.find('p').text()).toEqual('House rules are...');
   });
 });
