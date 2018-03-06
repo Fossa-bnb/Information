e3075688ccf4265f9439f37b635ef79b61c8f037
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:21:59 -0800

    Add list item test

--- src/amenities.test.jsx
+++ src/amenities.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import Amenities from '../client/components/amenities';
+import Amenities from '../client/app/components/Amenities';
 
 describe('<Amenities /> Component', () => {
   it('should render a div', () => {
-    const wrapper = shallow(<Amenities />);
+    const wrapper = shallow(<Amenities rooms="" />);
     expect(wrapper.is('div')).toEqual(true);
+  });
+  it('renders the amenity li item given', () => {
+    const room = { wifi: true };
+    const wrapper = shallow(<Amenities room={room} />);
+    // need to figure out how to test this componit
+    expect(wrapper.find('li').text()).toEqual('Wifi');
   });
 });
