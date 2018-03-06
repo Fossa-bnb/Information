25bc2b5ecc8e160c0aa55c9bf466492f417a8471
EvaFro <frolli.erin@gmail.com>
2018-03-06 12:24:08 -0800

    add a GET request for the roomObj

--- client/app/IndexPart1.jsx
+++ client/app/IndexPart1.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import RenderDOM from 'react-dom';
+import $ from 'jquery';
 import Amenities from './components/Amenities';
 import CancelPolicy from './components/CancelPolicy';
 import HostImage from './components/HostImage';
 import HouseRules from './components/HouseRules';
 import RoomDescription from './components/RoomDescription';
 import RoomHeader from './components/RoomHeader';
-import fetchDB from '../database/fetchRoom';
 
 class App extends React.Component {
   constructor(props) {
@@ -17,9 +17,20 @@ class App extends React.Component {
     };
   }
 
-  fetchRoom(roomObj) {
-    this.setState({
-      room: roomObj,
+  fetchRoom() {
+    const roomID = this.props.id;
+    $.ajax({
+      url: `/${roomID}`,
+      method: 'GET',
+      success: ({ roomObj }) => {
+        console.log(`SUCCESS - GET roomObj: ${roomObj.id}`);
+        this.setState({
+          room: roomObj,
+        });
+      },
+      error: (err) => {
+        console.log(`ERROR - Bad GET: ${err}`);
+      },
     });
   }
 
