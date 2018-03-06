bc752e335b014d477168a33c13520eedded848bb
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:09:13 -0800

    Add host info paragraph test

--- src/hostInfo.test.jsx
+++ src/hostInfo.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import HostInfo from '../client/components/hostInfo';
+import HostInfo from '../client/app/components/HostInfo';
 
 describe('<HostInfo /> Component', () => {
   it('should render a title', () => {
     const wrapper = shallow(<HostInfo />);
     expect(wrapper.is('h1')).toEqual(true);
+  });
+  it('renders the host info paragraph given', () => {
+    const room = { hostInfo: 'host info...' };
+    const wrapper = shallow(<HostInfo room={room} />);
+    // need to figure out how to test this componit
+    expect(wrapper.find('p').text()).toEqual('host info...');
   });
 });
