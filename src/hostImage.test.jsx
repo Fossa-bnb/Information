9a9627376797151d2f629207e8cc71af2c34a5da
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:13:19 -0800

    Adde renders host image test

--- src/hostImage.test.jsx
+++ src/hostImage.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import HostImage from '../client/components/hostImage';
+import HostImage from '../client/app/components/HostImage';
 
 describe('<HostImage /> Component', () => {
   it('should render a div', () => {
-    const wrapper = shallow(<HostImage />);
+    const wrapper = shallow(<HostImage room="" />);
     expect(wrapper.is('div')).toEqual(true);
+  });
+  it('renders the host image', () => {
+    const room = { hostImage: 'hostImage.jpg' };
+    const wrapper = shallow(<HostImage room={room} />);
+    // need to figure out how to test this componit
+    expect(wrapper.find('img').text()).toEqual('hostImage.jpg');
   });
 });
