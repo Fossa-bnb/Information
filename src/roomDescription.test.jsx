7de36b1eab0435aac563f3a83ac18e6b09ac5866
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:04:33 -0800

    Add room description test

--- src/roomDescription.test.jsx
+++ src/roomDescription.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import RoomDescription from '../client/components/roomDescription';
+import RoomDescription from '../client/app/components/RoomDescription';
 
 describe('<RoomDescription /> Component', () => {
   it('should render a div ', () => {
     const wrapper = shallow(<RoomDescription />);
     expect(wrapper.is('div')).toEqual(true);
+  });
+  it('renders the room description paragraph given', () => {
+    const room = { roomDescription: 'Room description is...' };
+    const wrapper = shallow(<RoomDescription room={room} />);
+    // need to figure out how to test this componit
+    expect(wrapper.find('p').text()).toEqual('Room description is...');
   });
 });
