ac336ddafaba85fdf28b264ac542f00d987c1e18
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:26:59 -0800

    Add room header li test

--- src/roomHeader.test.jsx
+++ src/roomHeader.test.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import RoomHeader from '../client/components/roomHeader';
+import RoomHeader from '../client/app/components/RoomHeader';
 
 describe('<RoomHeader /> Component', () => {
   it('should render a title', () => {
-    const wrapper = shallow(<RoomHeader />);
+    const wrapper = shallow(<RoomHeader room="" />);
     expect(wrapper.is('h1')).toEqual(true);
+  });
+  it('renders the room header li item given', () => {
+    const room = { guests: 2 };
+    const wrapper = shallow(<RoomHeader room={room} />);
+    expect(wrapper.find('li').text()).toEqual('2');
   });
 });
