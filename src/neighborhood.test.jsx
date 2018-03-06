8c90ca48a6a61172cde46bc074963561941ee764
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:06:19 -0800

    add neighborhood description paragraph test

--- src/neighborhood.test.jsx
+++ src/neighborhood.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import Neighborhood from '../client/components/neighborhood';
+import Neighborhood from '../client/app/components/Neighborhood';
 
 describe('<Neighborhood /> Component', () => {
   it('should render a div', () => {
     const wrapper = shallow(<Neighborhood />);
     expect(wrapper.is('div')).toEqual(true);
+  });
+  it('renders the neighborhood description paragraph given', () => {
+    const room = { neighborhood: 'neighborhood description...' };
+    const wrapper = shallow(<Neighborhood room={room} />);
+    // need to figure out how to test this componit
+    expect(wrapper.find('p').text()).toEqual('neighborhood description...');
   });
 });
