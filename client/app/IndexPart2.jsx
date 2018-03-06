676b9c5b8234dd532a5b891a1372f8764bd9c29a
EvaFro <frolli.erin@gmail.com>
2018-03-06 11:27:49 -0800

    cange comp from statless to statfull & fill out

--- client/app/IndexPart2.jsx
+++ client/app/IndexPart2.jsx
@@ -4,12 +4,29 @@ import HostInfo from './components/HostInfo';
 import Neighborhood from './components/Neighborhood';
 
 
-const App = () => (
-  <div>
-    <h1>Further Info</h1>
-    <HostInfo />
-    <Neighborhood />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomID: props.room.id,
+      room: {}
+    };
+  }
 
-RenderDOM.render(<App />, document.getElementById('app2'));
+  fetchRoom (roomID) {
+    this.setState({
+      this.state.room: {} // will fix to do a fetch request to get this object
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <HostInfo room={room} />
+      <Neighborhood room={room} />
+    </div>
+    )
+  }
+};
+
+RenderDOM.render(<App roomId={{ id: 1 }} />, document.getElementById('app2'));
