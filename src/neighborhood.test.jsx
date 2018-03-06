e217cfc7087d05b007d9507626181b29f22b1490
EvaFro <frolli.erin@gmail.com>
2018-03-05 23:29:46 -0800

    add room prop to 'should render div'

--- src/neighborhood.test.jsx
+++ src/neighborhood.test.jsx
@@ -3,7 +3,7 @@ import Neighborhood from '../client/app/components/Neighborhood';
 
 describe('<Neighborhood /> Component', () => {
   it('should render a div', () => {
-    const wrapper = shallow(<Neighborhood />);
+    const wrapper = shallow(<Neighborhood room="" />);
     expect(wrapper.is('div')).toEqual(true);
   });
   it('renders the neighborhood description paragraph given', () => {
