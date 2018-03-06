7042c120ecb8621e90916dde55cee41f9e29207d
EvaFro <frolli.erin@gmail.com>
2018-03-06 00:12:36 -0800

    refactored hostImage test & component

--- src/hostImage.test.jsx
+++ src/hostImage.test.jsx
@@ -2,14 +2,8 @@ import React from 'react';
 import HostImage from '../client/app/components/HostImage';
 
 describe('<HostImage /> Component', () => {
-  it('should render a div', () => {
+  it('should render hostImage', () => {
     const wrapper = shallow(<HostImage room="" />);
-    expect(wrapper.is('div')).toEqual(true);
-  });
-  it('renders the host image', () => {
-    const room = { hostImage: 'hostImage.jpg' };
-    const wrapper = shallow(<HostImage room={room} />);
-    // need to figure out how to test this componit
-    expect(wrapper.find('img').text()).toEqual('hostImage.jpg');
+    expect(wrapper.is('img')).toEqual(true);
   });
 });
