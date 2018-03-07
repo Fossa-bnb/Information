34d7c38c2c616fad84db9459974a5c8ac8149325
EvaFro <frolli.erin@gmail.com>
2018-03-06 21:16:18 -0800

    Add a break line inbetween city and list items

--- client/app/components/RoomHeader.jsx
+++ client/app/components/RoomHeader.jsx
@@ -9,6 +9,7 @@ const RoomHeader = (props) => {
       <div>{ props.roomHeader.roomType }</div>
       <h1>{ props.roomHeader.title }</h1>
       <div>{ props.roomHeader.city }</div>
+      <br />
       <li>{ `${props.roomHeader.guest} guests` }</li>
       <li>{ `${props.roomHeader.bed} ${bedWords}` }</li>
       <li>{ `${props.roomHeader.bath} ${bathWords}` }</li>
