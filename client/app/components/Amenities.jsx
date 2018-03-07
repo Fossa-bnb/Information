3639f87d260d6c33f3d07dd50c7a6d705dbb1367
EvaFro <frolli.erin@gmail.com>
2018-03-07 12:11:02 -0800

    add notes about what this script does

--- client/app/components/Amenities.jsx
+++ client/app/components/Amenities.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Amenities = (props) => {
   const amenitiesArray = props.amenities.amenitiesList;
+  // amenities[0] - amenities Name
+  // amenities[1] - ammenites exsists
+  // amenities[2] - ammenities popUp message
 
   return (
     <div>
@@ -9,14 +12,17 @@ const Amenities = (props) => {
       <div>
         {
           amenitiesArray.map((amenity) => {
-            let title = '';
+            let popUpDescrip = '';
+            // not all of the amenities have popUp descriptions
             if (amenity[2] !== undefined) {
-              title = amenity[2];
+              // if has a popUp description
+              popUpDescrip = amenity[2];
             }
             if (amenity[1] === true) {
-              return <li title={title}>{ amenity[0] } </li>;
+              // popUpDescrip tag renders a pop up with text when hovered over the item.
+              return <li tittle={popUpDescrip}>{ amenity[0] } </li>;
             }
-            return <li title={title}><s> { amenity[0] } </s> </li>;
+            return <li title={popUpDescrip}><s> { amenity[0] } </s> </li>;
           })
         }
       </div>
