6eac937782be09fef5093db0c2e63eb412930f55
EvaFro <frolli.erin@gmail.com>
2018-03-07 14:25:11 -0800

    Add tests 2 check data is passed down 2 child node

--- src/roomDescription.test.jsx
+++ src/roomDescription.test.jsx
@@ -3,12 +3,42 @@ import RoomDescription from '../client/app/components/RoomDescription';
 
 describe('<RoomDescription /> Component', () => {
   it('should render a div ', () => {
-    const wrapper = shallow(<RoomDescription roomDescription="" />);
+    const wrapper = shallow(<RoomDescription roomInfo="" />);
     expect(wrapper.is('div')).toEqual(true);
   });
-  it('renders the room description paragraph given', () => {
-    const roomDescription = 'Room description is...';
-    const wrapper = shallow(<RoomDescription roomDescription={roomDescription} />);
-    expect(wrapper.find('p').text()).toEqual('Room description is...');
+  it('renders the Room description paragraph', () => {
+    const roomInfo = { roomDescription: 'Room description is...' };
+    const wrapper = mount(<RoomDescription roomInfo={roomInfo} />);
+    expect(wrapper.contains([
+      <p>Room description is...</p>,
+    ])).toEqual(true);
+  });
+  it('renders The Space description paragraph', () => {
+    const roomInfo = { spaceDescription: 'Space description is...' };
+    const wrapper = mount(<RoomDescription roomInfo={roomInfo} />);
+    expect(wrapper.contains([
+      <p>Space description is...</p>,
+    ])).toEqual(true);
+  });
+  it('renders the Interaction description paragraph', () => {
+    const roomInfo = { interactionDescription: 'Interaction description is...' };
+    const wrapper = mount(<RoomDescription roomInfo={roomInfo} />);
+    expect(wrapper.contains([
+      <p>Interaction description is...</p>,
+    ])).toEqual(true);
+  });
+  it('renders the Guest Access description paragraph', () => {
+    const roomInfo = { guestAccessDescription: 'Guest description is...' };
+    const wrapper = mount(<RoomDescription roomInfo={roomInfo} />);
+    expect(wrapper.contains([
+      <p>Guest description is...</p>,
+    ])).toEqual(true);
+  });
+  it('renders the Other Things description paragraph', () => {
+    const roomInfo = { otherDescription: 'Other description is...' };
+    const wrapper = mount(<RoomDescription roomInfo={roomInfo} />);
+    expect(wrapper.contains([
+      <p>Other description is...</p>,
+    ])).toEqual(true);
   });
 });
