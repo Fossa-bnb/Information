8c83101e51ab793a9e7e0276d0a6852a77b0702e
EvaFro <frolli.erin@gmail.com>
2018-03-06 21:47:09 -0800

    dinamicaly render amenities list

--- client/app/components/Amenities.jsx
+++ client/app/components/Amenities.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const Amenities = (props) => {
-  const listItem = props.amenities.wifi === true ? 'Wifi' : '';
+  const amenitiesArray = props.amenities.amenitiesList;
+
   return (
     <div>
-      <h1>Amenities Component</h1>
-      <li>{ listItem }</li>
+      <h2>Amenities</h2>
+      <div>
+        {
+          amenitiesArray.map((amenity) => {
+            if (amenity[1] === true) {
+              return <li>{ amenity[0] } </li>;
+            }
+              return <li><s> { amenity[0] } </s> </li>;
+          })
+        }
+      </div>
     </div>
   );
 };
