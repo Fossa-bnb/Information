38729d4cafd0d875f5a41abdb65fad244dbd1929
EvaFro <frolli.erin@gmail.com>
2018-03-06 20:58:09 -0800

    change className to "boldText"

--- client/app/components/HostInfo.jsx
+++ client/app/components/HostInfo.jsx
@@ -10,13 +10,13 @@ const HostInfo = (props) => {
       <hr />
       <p>{ props.hostInfo.hostDescription }</p>
       <div>Languages:</div>
-      <div className="hostStats"> {`   ${props.hostInfo.hostLanguage}`}</div>
+      <div className="boldText"> {`   ${props.hostInfo.hostLanguage}`}</div>
       <div>Response rate:</div>
-      <div className="hostStats"> {`   ${props.hostInfo.hostResponceRate}%`}</div>
+      <div className="boldText"> {`   ${props.hostInfo.hostResponceRate}%`}</div>
       <div>Response time:</div>
-      <div className="hostStats"> {`   ${props.hostInfo.hostResponceTime}`}</div>
+      <div className="boldText"> {`   ${props.hostInfo.hostResponceTime}`}</div>
       <hr />
-      <div className="hostStats">Always communicate through Airbnb · </div>
+      <div className="boldText">Always communicate through Airbnb · </div>
       <div>{ props.hostInfo.hostWarning }</div>
       <hr />
     </div>
